f4b98e7b1b92c585c961ddd8bd6a6b8ebcb60c90
Sanux380 <Sanux080@gmail.com>
2025-06-18 09:45:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙱𝚄𝙽𝙽𝚈-𝙼𝙳=", // පෙයා සයිඩ් එක තාම හදලා නැති නිසා කැමති සීසන් එකක් ඇඩ් කරන්න
+SESSION_ID: process.env.SESSION_ID || "𝚀𝚄𝚄𝙴𝙽-𝚇 𝙼𝙳=", // පෙයා සයිඩ් එක තාම හදලා නැති නිසා කැමති සීසන් එකක් ඇඩ් කරන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/cp08zo.jpg",
 BOT_NAME: process.env.BOT_NAME || "𝙱𝚄𝙽𝙽𝚈-𝙼𝙳",
